4cea9f941e3b8ab1eacf63d84718bfc3550a1d56
Akshat <axxatagrawal@gmail.com>
2022-02-18 19:14:23 +0530

    Commited

---
+++ public/js.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll',()=>{
+    console.log("Scrolled");
+    const scrolld = window.scrollY;
+    const add = document.get 
+    console.log(scrolld);
+    if(scrolld>82){
+
+    }
+})
+
+var typed = new Typed(".typing",{
+    strings: ["Web Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+console.log("hello world i m testing");
+
+var typed = new Typed(".typin",{
+    strings: ["a Web Developer", "a Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
